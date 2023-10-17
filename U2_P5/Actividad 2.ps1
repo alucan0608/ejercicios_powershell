aeddaaeb4ed7840af381a7e4410a35242559003a
alucan0608 <alucan0608@ieselcaminas.org>
2023-10-17 13:46:19 +0200

    U2_P5

---
+++ U2_P5/Actividad 2.ps1
@@ -0,0 +1,16 @@
+﻿New-Item -Path C:\Empresa -ItemType Directory
+
+$departamento = Import-Csv -Path C:\Users\Administrador\Desktop\departamentos.csv -Delimiter ";"
+
+foreach ($dep in $departamento) {
+    $nomdep = $dep.departamento
+
+    New-Item -Path C:\Empresa\$nomdep -ItemType Directory
+}
+
+
+foreach ($dep in $departamento) {
+    $nomdep = $dep.departamento
+
+    New-SmbShare -Path C:\Empresa\$nomdep -Name $nomdep -ChangeAccess $nomdep -FullAccess "Administradores" -ReadAccess "Usuarios del dominio"
+}
