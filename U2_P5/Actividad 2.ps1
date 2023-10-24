d31f8f48ea6adc8d83a381c47012177cdea7e4e1
alucan0608 <alucan0608@ieselcaminas.org>
2023-10-24 12:14:16 +0200

    U2_P5

--- U2_P5/Actividad 2.ps1
+++ U2_P5/Actividad 2.ps1
@@ -14,3 +14,25 @@ foreach ($dep in $departamento) {
 
     New-SmbShare -Path C:\Empresa\$nomdep -Name $nomdep -ChangeAccess $nomdep -FullAccess "Administradores" -ReadAccess "Usuarios del dominio"
 }
+
+
+#Permisos NTFS
+foreach ($dep in $departamento) {
+    $acl = Get-Acl -Path C:\Empresa\$($dep.departamento)
+
+    $acl.SetAccessRuleProtection($true, $false)
+
+    $permiso = 'Administradores','FullControl','ContainerInherit,ObjectInherit','None','Allow'
+    $ace = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule -ArgumentList $permiso
+    $acl.SetAccessRule($ace)
+
+    $permiso = "$($dep.departamento)",'Modify','ContainerInherit,ObjectInherit','None','Allow'
+    $ace = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule -ArgumentList $permiso
+    $acl.SetAccessRule($ace)
+
+    $permiso = 'Usuarios del dominio','Read','ContainerInherit,ObjectInherit','None','Allow'
+    $ace = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule -ArgumentList $permiso
+    $acl.SetAccessRule($ace)
+    $acl | Set-Acl -Path C:\Empresa\$($dep.departamento)
+    $acl.Access | Format-Table
+}
