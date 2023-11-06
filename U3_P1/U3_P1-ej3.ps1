591b73f2b84cf2efaeff34fee7177dd1f40ddfbd
alucan0608 <alucan0608@ieselcaminas.org>
2023-11-06 09:52:00 +0100

    U3_P1

---
+++ U3_P1/U3_P1-ej3.ps1
@@ -0,0 +1,9 @@
+﻿$usuario = Read-Host "Escribe un usuario para borrar todos sus procesos"
+
+$proceso = Get-Process -IncludeUserName | where UserName -eq $usuario
+
+foreach ($proc in $proceso) {
+
+    Stop-Process -Id $proc.Id
+
+}
